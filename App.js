a0b07474ff52f707d5e5197bb8c14e0b3666740e
Julia Zhou <juliazhouism@gmail.com>
2018-09-22 12:12:29 -0700

    rendering data using axios, setting up yelp api

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList, ActivityIndicator, Text, View } from "react-native";
+import axios from "axios";
 
-export default class App extends React.Component {
-  render() {
+export default class FetchExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    const testUrl = "https://facebook.github.io/react-native/movies.json";
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      axios
+        .get(testUrl)
+        // .then(response => console.log(response.data.movies));
+
+        .then(responseJson => {
+          this.setState(
+            {
+              isLoading: false,
+              dataSource: responseJson.data.movies
+            },
+            function() {}
+          );
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+    );
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+          keyExtractor={({ id }, index) => id}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
