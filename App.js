7ca3f582cb953a2fb7551167bac6dabe79e276d5
Julia Zhou <juliazhouism@gmail.com>
2018-09-22 16:14:33 -0700

    yelp search by phone working and rendering data on screen

--- App.js
+++ App.js
@@ -9,77 +9,57 @@ const config = {
     Authorization: authToken
   },
   params: {
-    phone: "+14157492060"
+    phone: "+16046858817"
   }
 };
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: {}
+    };
+  }
+
   componentWillMount() {
     axios
       .get("https://api.yelp.com/v3/businesses/search/phone", config)
-      .then(response => console.log(response.data.businesses))
+      // .then(response => console.log(response.data.businesses[0]))
+      .then(response => {
+        this.setState({
+          isLoading: false,
+          data: response.data.businesses
+        });
+      })
       .catch(function(error) {
         console.log(error);
       });
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
-      <View>
-        <Text> My first yelp authentication request </Text>
+      // console.log(this.state.data)
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <Text>
+              Phone: {item.display_phone}, Owner: {item.name}, Price Range:{" "}
+              {item.price}, Address: {item.location.address1}, City:{" "}
+              {item.location.city}
+            </Text>
+          )}
+          keyExtractor={item => item.phone}
+        />
       </View>
     );
   }
 }
-
-// export default class FetchExample extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { isLoading: true };
-//   }
-
-//   componentDidMount() {
-//     const testUrl = "https://facebook.github.io/react-native/movies.json";
-//     return (
-//       axios
-//         .get(testUrl)
-//         // .then(response => console.log(response.data.movies));
-//         .then(responseJson => {
-//           this.setState(
-//             {
-//               isLoading: false,
-//               dataSource: responseJson.data.movies
-//             },
-//             function() {}
-//           );
-//         })
-//         .catch(function(error) {
-//           console.log(error);
-//         })
-//     );
-//   }
-
-//   render() {
-//     if (this.state.isLoading) {
-//       return (
-//         <View style={{ flex: 1, padding: 20 }}>
-//           <ActivityIndicator />
-//         </View>
-//       );
-//     }
-
-//     return (
-//       <View style={{ flex: 1, paddingTop: 20 }}>
-//         <FlatList
-//           data={this.state.dataSource}
-//           renderItem={({ item }) => (
-//             <Text>
-//               {item.title}, {item.releaseYear}
-//             </Text>
-//           )}
-//           keyExtractor={({ id }, index) => id}
-//         />
-//       </View>
-//     );
-//   }
-// }
