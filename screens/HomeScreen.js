10471c7fd31261a5519c1e20471d0894653f0e27
Julia Zhou <juliazhouism@gmail.com>
2018-09-23 13:04:18 -0700

    root stack created in app.js

---
+++ screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React, { Components } from "react";
+import { Text, View, Button } from "react-native";
+
+export default class HomeScreen extends React.Component {
+  render() {
+    return <View />;
+  }
+}
